23fea36dc8cf75d10ceea9125fe7583b9bfe529e
Bala Pramithran <pramithran2005@gmail.com>
2024-12-27 17:03:22 +0530

    End sprint 1 and finalize features

--- SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.component.ts
+++ SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule,} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { InstructorAppliedComponent } from './instructor/components/instructor-applied/instructor-applied.component';
+import { ListOfApplicantsComponent } from './instructor/components/Add_Instructor/list-of-applicants/list-of-applicants.component';
+import { NavBar2Component } from "./instructor/components/nav-bar2/nav-bar2.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule, HttpClientModule, NavBar2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.routes.ts
+++ SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { RegistrationFormComponent } from './instructor/components/Add_Instructor/instructor-registration/instructor-registration/registration-form/registration-form.component';
+import { ListOfApplicantsComponent } from './instructor/components/Add_Instructor/list-of-applicants/list-of-applicants.component';
+import { InstructorApplyComponent } from './instructor/components/instructor-apply/instructor-apply.component';
+import { FormsubmittedComponent } from './instructor/components/formsubmitted/formsubmitted.component';
+import { InstructorAppliedComponent } from './instructor/components/instructor-applied/instructor-applied.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:InstructorApplyComponent},
+    {path:'form', component:RegistrationFormComponent},
+    {
+    path:'applicants',component:ListOfApplicantsComponent
+    },{
+    path:'successfully-submitted',component:FormsubmittedComponent
+    },
+    {path:'applicants-details',component: InstructorAppliedComponent},
+    {path:'available-tutors',component: ListOfApplicantsComponent}
+];
