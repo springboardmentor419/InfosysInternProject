b05bb79e2aca1b08e1dc7cd005cde1bd3226ee65
springboardmentor419 <springboardmentor419@gmail.com>
2025-01-01 16:35:05 +0530

    Update app.routes.ts

--- SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.routes.ts
+++ SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.routes.ts
@@ -19,35 +19,59 @@ import { UpdateProfileComponent } from './candidates/components/update-profile/u
 import { ChangePasswordComponent } from './candidates/components/change-password/change-password.component';
 import { AdminCreateCourseComponent } from './course/components/admin-create-course/admin-create-course.component';
 import { AdminViewCoursesComponent } from './course/components/admin-view-courses/admin-view-courses.component';
+import { CourseComponent } from './assessmentgrading/components/instructor/course/course.component';
+import { AssessmentComponent } from './assessmentgrading/components/instructor/assessment/assessment.component';
+import { ScheduleAssessmentComponent } from './assessmentgrading/components/instructor/schedule-assessment/schedule-assessment.component';
+import { AssessmentDashComponent } from './assessmentgrading/components/candidate/assessment-dash/assessment-dash.component';
+import { candeactivateGuard } from './assessmentgrading/components/candidate/candeactivate.guard';
+import { InassessmentComponent } from './assessmentgrading/components/candidate/inassessment/inassessment.component';
+import { ReviewComponent } from './assessmentgrading/components/candidate/review/review.component';
+import { ReportComponent } from './assessmentgrading/components/candidate/report/report.component';
 
 
 export const routes: Routes = [
-//  {path: '', component:InstructorApplyComponent},
-    {path:'form', component:RegistrationFormComponent},
-    {path:'applicants',component:ListOfApplicantsComponent},
-    {path:'successfully-submitted',component:FormsubmittedComponent},
-    {path:'applicants-details',component: InstructorAppliedComponent},
-    {path:'available-tutors',component: ListOfApplicantsComponent},
-    
+//Team1
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'apply', component: ApplyComponent },
     { path: 'admin', component: AdminComponent, canActivate:[routeGuard] },
     { path: 'candidate', component: CandidateComponent, canActivate:[routeGuard]},
     { path: 'instructor', component:  InstructorComponent, canActivate:[routeGuard] },
-//  { path: '', redirectTo: 'login', pathMatch: 'full' },
-//  { path: '**', redirectTo: 'login', },
+  //{ path: '', redirectTo: 'login', pathMatch: 'full' },
+  //{ path: '**', redirectTo: 'login', },
+
+ //Team2
+ //{ path: '', redirectTo: 'view-courses', pathMatch: 'full' },
+  { path: 'create-course', component: AdminCreateCourseComponent },
+  { path: 'view-courses', component: AdminViewCoursesComponent },   
+
+//Team3
+ 
+  //{path: '', component:InstructorApplyComponent},
+    {path:'form', component:RegistrationFormComponent},
+    {path:'applicants',component:ListOfApplicantsComponent},
+    {path:'successfully-submitted',component:FormsubmittedComponent},
+    {path:'applicants-details',component: InstructorAppliedComponent},
+    {path:'available-tutors',component: ListOfApplicantsComponent},
+
+//Team4
     
-// { path: '', redirectTo: '/register', pathMatch: 'full' },
+//{ path: '', redirectTo: '/register', pathMatch: 'full' },
   { path: 'login', component: CandidateLoginComponent },
   { path: 'register', component: CandidateRegistrationComponent },
   { path: 'change-password', component: ChangePasswordComponent },
   { path: 'dashboard', component: CandidateDashboardComponent },
   { path: 'update-profile', component: UpdateProfileComponent },
-    
-  // { path: '', redirectTo: 'view-courses', pathMatch: 'full' },
-  { path: 'create-course', component: AdminCreateCourseComponent },
-  { path: 'view-courses', component: AdminViewCoursesComponent }
+
+//Team5
+    {path : 'course',component : CourseComponent},
+    {path : 'assessment/:id',component : AssessmentComponent},
+    {path : 'schedule',component : ScheduleAssessmentComponent},
+    {path : 'assessmentdash',component : AssessmentDashComponent},
+    {path : 'insideassessment',component : InassessmentComponent,canDeactivate : [candeactivateGuard],},
+    {path : 'review',component : ReviewComponent},
+    {path : 'report',component : ReportComponent} 
+  
 
 ];
 
